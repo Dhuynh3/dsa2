0a3eb3efd5d3fdb3da7b42eb199bc0ce949234a6
Dat Huynh <ahythel@gmail.com>
2023-09-05 21:57:02 -0500

    Update

---
+++ hashtable.py
@@ -0,0 +1,4 @@
+# Chaining Hash Table with Linked List Implementation
+class HashTable:
+    def __init__(self, size=16):
+        
---
+++ linkedlist.py
@@ -0,0 +1,16 @@
+# Singly Linked List Implementation
+
+class ListNode: 
+    def __init__(self):
+        self.truck_id
+        self.package
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    
+    def insert_node(self, new_list_node):
+        return
